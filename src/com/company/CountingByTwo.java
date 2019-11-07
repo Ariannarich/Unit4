02303ed3a675c71c5b7f60a8df47eb481fd105b3
richaar <arianna.richardson@ssfs.org>
2019-11-07 17:33:03 -0500

    Count by 2 done

---
+++ src/com/company/CountingByTwo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class CountingByTwo {
+    public static void main(String[] args) {
+        int num = 2;
+        while (num <= 20)
+        {
+       System.out.print(num + " ");
+            num = num + 2;
+
+        }
+
+    }
+}
