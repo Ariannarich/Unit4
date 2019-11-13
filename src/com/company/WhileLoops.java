cc69ee2113928f13497f68fb59bc852911140ec4
richaar <arianna.richardson@ssfs.org>
2019-11-13 14:41:50 -0500

    factor, fromhere to there done, count pos not done

--- src/com/company/WhileLoops.java
+++ src/com/company/WhileLoops.java
@@ -19,35 +19,51 @@ public class WhileLoops {
     }
 
 
-public static String Factor( int y)
-{ int x = 1;
-    while(x < y)
-    {
-       if (y % x == 0)
-          return (x + " ");
-       x++;
+    public static String Factor(int y) {
+        int x = 1;
+        while (x < y) {
+            if (y % x == 0)
+                return (x + " ");
+            x++;
+        }
+        return String.valueOf(x);
     }
-    return String.valueOf(x);
-}
+
     public static String countPosAndNeg() {
-        System.out.println("Enter a positive or negative number or 0 to quit: ");
+
         int num = input.nextInt();
         int pos = 0;
         int neg = 0;
         while (num != 0) {
-         if (num >= 1)
+            if (num >= 1)
                 pos++;
-              else if (num <= -1)
-                    neg++;
-              System.out.println("Enter a positive or negative number or 0 to quit:");
+            else if (num <= -1)
+                neg++;
+            System.out.println("Enter a positive or negative number or 0 to quit:");
             num = input.nextInt();
-            }
-
-        return  "There were " + neg + " negative and " + pos + " positive numbers";
         }
 
+        return "There were " + neg + " negative and " + pos + " positive numbers";
+    }
+
+    public static String findMinAndMax() {
+        int max= Integer.MIN_VALUE;
+        int min= Integer.MAX_VALUE;
+        int num = 1;
+        int num2 = input.nextInt();
+        while (num <= 4) {
+            System.out.println("Enter number: ");
+            num2 = input.nextInt();
+            num++;
+        }
+
+        if ( num2 > max )
+            max = num2;
+        return "max is";
+    }
     public static void main(String[] args) {
 
-        System.out.println(countPosAndNeg());
+
+        System.out.println(findMinAndMax());
     }
 }
