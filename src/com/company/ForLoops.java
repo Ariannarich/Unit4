a98bfbf9d641887d94da8bc3ef6d802d4b56d7ab
Ariannarich <arianna.richardson@ssfs.org>
2019-11-20 23:19:26 -0500

    for loops done

--- src/com/company/ForLoops.java
+++ src/com/company/ForLoops.java
@@ -4,37 +4,58 @@ public class ForLoops {
 
     public static String printingHashTags(int num) {
         String hash = "";
-        for (int count = 0; count <= num; count++)
-            hash = "#";
+        for (int count = 1; count <= num; count++)
+            hash += "#";
         return hash;
     }
 
-    public static String countDown(int a, int b) {
+    public static String countDown(int a, int b)
+    {
         String str = "";
-        if (a < b) {
-            for (int count = a; count <= b; count++)
-                str = count + " ";
-        } else
-            str = "";
-        return str;
+  if (a <= b) {
+        for (int count = b; count >= a; count--)
+            str  += count + " ";
+  }
+  else if  (b < a)
+  {
+      str = "";
+  }
+  return str;
     }
-
     public static String countDown2(int a, int b) {
         String str = "";
-        if (a < b) {
-            for (int count = a; count <= b; count++)
-                str = count + " ";
-        } else if (b > a) {
-            for (int count = a; count <= b; count++)
-                str = count + " ";
+        if (a >= b )  {
+            for (int count = a; count >= b; count--)
+                str  += count + " ";
         }
-        return str;
+        else if (b >= a)
+        {
+            for (int count = b; count >= a; count--)
+                str  += count + " ";
     }
-   public static int sumNumbers(int num1, int num2)
-   {
-       for (int count = num1; count <= num2; count++)
-           count += num1;
-return num1;
 
+        return str;
+}
+
+   public static int sumNumbers(int num1, int num2)
+   {int sum=0;
+       if (num2 > num1)
+
+       {
+            for (int count = num1; count <= num2; count++)
+          sum += count;
+       }
+       else if (num1 > num2)
+       {
+           for (int count = num2; count <= num1; count++)
+           sum += count;
+       }
+return sum;
    }
+    public static void main(String[] args) {
+
+        System.out.println(countDown(0,10));
+
+    }
+
 }
