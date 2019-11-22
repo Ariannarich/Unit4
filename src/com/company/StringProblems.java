ef7c18bdf99fc65fed575f3b6a37016508cbfe2d
richaar <arianna.richardson@ssfs.org>
2019-11-22 15:58:28 -0500

    finished everything but piglet comments

--- src/com/company/StringProblems.java
+++ src/com/company/StringProblems.java
@@ -1,104 +1,81 @@
 package com.company;
 
 public class StringProblems {
-    public static String printTheLetters(String s)
-    {
+    public static String printTheLetters(String s) {
         String str = "";
 
-for (int count = 0 ; count < s.length(); count++)
-    str += s.substring(count, count+ 1) + "\n" ;
-return str;
-}
-    public static String everyOtherLetter(String s)
-    {
+        for (int count = 0; count < s.length(); count++)
+            str += s.substring(count, count + 1) + "\n";
+        return str;
+    }
+
+    public static String everyOtherLetter(String s) {
         String str = "";
         int count = 0;
 
-        while (count < s.length())
-        {
-            if (count % 2 == 0 || count == 0)
-            {
-                str += s.substring(count, count+ 1);
-            count++;}
-            else
+        while (count < s.length()) {
+            if (count % 2 == 0 || count == 0) {
+                str += s.substring(count, count + 1);
+                count++;
+            } else
                 count++;
         }
         return str;
     }
-    public static int countTheVowels(String s)
-    {
-    int vowels = 0;
-        int count=0;
 
-        while ( count < s.length())
-        {
+    public static int countTheVowels(String s) {
+        int vowels = 0;
+        int count = 0;
 
-            if ('a'== (s.charAt(count)) || 'A'== (s.charAt(count))) {
+        while (count < s.length()) {
+
+            if ('a' == (s.charAt(count)) || 'A' == (s.charAt(count))) {
                 vowels++;
                 count++;
-            }
-            else if ('e'== (s.charAt(count)) || 'E'== (s.charAt(count))) {
+            } else if ('e' == (s.charAt(count)) || 'E' == (s.charAt(count))) {
                 vowels++;
                 count++;
-            }
-            else if ('i'== (s.charAt(count)) || 'I'== (s.charAt(count))) {
+            } else if ('i' == (s.charAt(count)) || 'I' == (s.charAt(count))) {
                 vowels++;
                 count++;
-            }
-            else if ('o' == (s.charAt(count)) || 'O' ==(s.charAt(count))) {
+            } else if ('o' == (s.charAt(count)) || 'O' == (s.charAt(count))) {
                 vowels++;
                 count++;
-            }
-            else if ('u'== (s.charAt(count)) || 'U' == (s.charAt(count))) {
+            } else if ('u' == (s.charAt(count)) || 'U' == (s.charAt(count))) {
                 vowels++;
                 count++;
-            }
-            else
+            } else
                 count++;
 
-    }
+        }
         return vowels;
     }
-    public static String differentStrings(String s1, String s2)
-    {
 
-        int count=0;
-        int length1 = s1.length()-1;
-        int length2 = s2.length()-1;
-        String str= "";
-        String string1 = s1.substring(count,count +1);
-        String string2 = s2.substring(count,count+ 1);
+    public static String differentStrings(String s1, String s2) {
 
-        if (length1 <= length2 || length2 <= length1)  {
+        int count = 0;
+        int length1 = s1.length() - 1;
+        int length2 = s2.length() - 1;
+        String str = "";
 
-             if (!string1.equals(string2))
-             {
-                 while (count <= length2 || count <= length2)
-                 {
-                    s1.substring(count,count +1);
-                    s2.substring(count,count+ 1);
-                         count++;
-                 }
-                     str =  s1 + "and "+ s2 + "are not the same. They differ at letter number " + count;
-             }
-
-             else if (string1.equals(string2))
+        if (length1 < length2)
+            s1 += " ";
+        else if (length2 < length1)
+            s2 += " ";
+         if (s1.equals(s2))
+           str = s1 +" and "+ s2  + " are the same";
+           else
             {
-                while (count <= length2 || count <= length2)
+                while (s1.substring(count, count + 1).equals(s2.substring(count, count + 1)))
                 {
-                    string1 = s1.substring(count,count +1);
-                    string2 = s2.substring(count,count+ 1);
-                        count++;
-                    }
-
-            }
-            str= s1+ " and "+ s2 +" are the same";
-
+                    count++;
                 }
-
-
+                count += 1;
+                str = s1 + "and " + s2 + " are not the same. They differ at letter number " + count;
+                }
         return str;
     }
+
     public static boolean isPalindrome(String s)
     {
         int count = 0;
