3be3abbb9d776cc852e0b599ccf5705d92cf9108
richaar <arianna.richardson@ssfs.org>
2019-12-03 08:59:49 -0500

    nested loops not done

---
+++ src/com/company/NestedLoops.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class NestedLoops {
+    public static String xSquare() {
+        String blank = "";
+
+        for (int x = 0; x <= 25; x++)
+            for(int =; x%5==0;y++)
+
+
+        return blank;
+    }
+
+    public static int xSquare2(int n) {
+       int blank = 0;
+        return blank;
+    }
+
+
+    public static int flippedTriangle(int n) {
+        int blank = 0;
+        return blank;
+    }
+    public static int multiplicationTable() {
+        int blank = 0;
+        return blank;
+    }
+}
+
+
