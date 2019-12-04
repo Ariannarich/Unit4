0bf4b25488e55e4d67869244b215bd0ea7660014
Ariannarich <arianna.richardson@ssfs.org>
2019-12-03 21:49:07 -0500

    finished Nested Loops

--- src/com/company/NestedLoops.java
+++ src/com/company/NestedLoops.java
@@ -1,30 +1,70 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class NestedLoops {
+    static Scanner input = new Scanner(System.in);
+
     public static String xSquare() {
         String blank = "";
 
-        for (int x = 0; x <= 25; x++)
-            for(int =; x%5==0;y++)
-
+        for(int x = 1; x <= 5; x++)
+            {
+                for(int y = 1;y <= 5;y++)
+                    blank += "X";
+                blank += "\n";
+            }
 
         return blank;
     }
 
-    public static int xSquare2(int n) {
-       int blank = 0;
+    public static String xSquare2(int n) {
+        String blank = "";
+
+        for(int x = 1; x <= n; x++)
+        {
+            for(int y = 1; y <= n; y++)
+                blank += "X";
+            blank += "\n";
+        }
         return blank;
     }
 
+    public static String flippedTriangle(int n) {
+        String blank = "";
+        for(int x = 1; x <= n; x++)
+        {
+            for(int y = n ; y >= x; y--)
+                blank += "*";
+            blank += "\n";
+        }
+        return blank;
+    }
+    public static String multiplicationTable() {
+        String blank = "";
+        for(int x = 1; x <= 9; x++)
+        {
+            for(int y = x; y <= 9 * x; y+=x)
+                {
+                    if (y < 10)
+                        blank += "  " + y;
+                    else
+                        blank += " " + y;
+                }
+            blank += "\n";
+        }
+        return blank;
 
-    public static int flippedTriangle(int n) {
-        int blank = 0;
-        return blank;
     }
-    public static int multiplicationTable() {
-        int blank = 0;
-        return blank;
+    public static void main(String[] args) {
+        int n = input.nextInt();
+        System.out.println(xSquare2(n));
+        int n2 = input.nextInt();
+        System.out.println(flippedTriangle(n2));
+
+
     }
+
 }
 
 
